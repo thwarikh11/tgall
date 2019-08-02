c8781e8825e5e0f884cc369f896d82829fb439f5
M.Furkan <byazade@yandex.com>
2019-08-02 19:38:23 +0300

    Update _core.py

--- plugins/_core.py
+++ plugins/_core.py
@@ -2,18 +2,32 @@
 # -*- coding: utf-8 -*-
 # (c) Shrimadhav U K
 
+import json
 # the logging things
 import logging
+import math
+import os
+import subprocess
+import time
+
+import requests
+
+import pyrogram
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+from helper_funcs.chat_base import TRChatBase
+from helper_funcs.display_progress import humanbytes, progress_for_pyrogram
+from helper_funcs.help_uploadbot import DetectFileSize, DownLoadFile
+from ParseMode import pyrogram
+# https://stackoverflow.com/a/37631799/4723940
+from PIL import Image
+# the Strings used for this "thing"
+from translation import Translation
+
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-import json
-import math
-import os
-import requests
-import subprocess
-import time
 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
@@ -21,20 +35,10 @@ if bool(os.environ.get("WEBHOOK", False)):
 else:
     from sample_config import Config
 
-# the Strings used for this "thing"
-from translation import Translation
 
-import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-from helper_funcs.chat_base import TRChatBase
-from helper_funcs.display_progress import progress_for_pyrogram, humanbytes
-from helper_funcs.help_uploadbot import DownLoadFile, DetectFileSize
 
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
-# https://stackoverflow.com/a/37631799/4723940
-from PIL import Image
 
 
 @pyrogram.Client.on_message(pyrogram.Filters.regex(pattern=".*http.*"))
